428e36d5b4a905e2020eea85644f177621230786
NGNikolay <georgiev.nikolaj@gmail.com>
2023-01-12 11:35:54 +0200

    added solutions

---
+++ basic-syntax/01-multiply-number-by-2.js
@@ -0,0 +1,10 @@
+function multiplyBy2(number) {
+
+    let result = number * 2;
+    console.log(result);
+
+}
+
+multiplyBy2(2);
+multiplyBy2(5);
+multiplyBy2(10);
---
+++ basic-syntax/02-student-information.js
@@ -0,0 +1,9 @@
+function studentInfo(name, age, averageGrade) {
+
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`);
+
+}
+
+studentInfo('John', 15, 5.54678);
+studentInfo('Steve', 16, 2.1426);
+studentInfo('Marry', 12, 6.00);
