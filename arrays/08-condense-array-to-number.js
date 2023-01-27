65ec01a8973dfed4f8ce0e99b40b0648a27ce290
NGNikolay <georgiev.nikolaj@gmail.com>
2023-01-27 12:31:47 +0200

    Create 08-condense-array-to-number.js

---
+++ arrays/08-condense-array-to-number.js
@@ -0,0 +1,24 @@
+function condense(array) {
+
+
+    if (array.length === 1) {
+        console.log(array.join(''));
+    } else {
+        for (let k = array.length - 1; k > 0; k--) {
+            let summingArray = [];
+            for (let i = 0; i < array.length - 1; i++) {
+                let result = 0;
+                result += (array[i] + array[i + 1]);
+                summingArray.push(result);
+            }
+            array = summingArray.slice(0);
+        }
+        console.log(array.join(''));
+    }
+
+
+}
+
+condense([2, 10, 3]);
+condense([5, 0, 4, 1, 2]);
+condense([1]);
