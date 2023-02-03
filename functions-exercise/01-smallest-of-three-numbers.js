660424e8e877c92cee67082b6ce05bd1437db00d
NGNikolay <georgiev.nikolaj@gmail.com>
2023-02-03 13:47:37 +0200

    Create 01-smallest-of-three-numbers.js

---
+++ functions-exercise/01-smallest-of-three-numbers.js
@@ -0,0 +1,20 @@
+function smallestOfThree(numOne, numTwo, numThree) {
+
+    let result = 0;
+    result = isSmallest(numOne, numTwo);
+    result = isSmallest(result, numThree);
+
+    console.log(result);
+
+    function isSmallest(a, b) {
+        let result = (a < b) ? a : b;
+        return result;
+    }
+
+
+}
+
+smallestOfThree(2, 5, 3);
+smallestOfThree(600, 342, 123);
+smallestOfThree(25, 21, 4);
+smallestOfThree(2, 2, 2);
