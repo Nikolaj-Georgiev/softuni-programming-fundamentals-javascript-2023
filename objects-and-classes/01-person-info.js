7dfead3d58542443afa7c904ed89b95673ef78c3
NGNikolay <georgiev.nikolaj@gmail.com>
2023-02-23 11:52:36 +0200

    Create 01-person-info.js

---
+++ objects-and-classes/01-person-info.js
@@ -0,0 +1,62 @@
+function personInfo(fName, lName, personAge) {
+
+    let person = {
+        firstName: fName,
+        lastName: lName,
+        age: personAge,
+    }
+
+    return person;
+
+}
+
+console.log(personInfo("Peter",
+    "Pan",
+    "20"
+));
+
+function secondPerson(firstName, lastName, age) {
+
+    let person = {
+        firstName,
+        lastName,
+        age,
+    }
+
+    return person;
+}
+
+secondPerson("Peter",
+    "Pan",
+    "20");
+
+function thirdPerson(fName, lName, age) {
+
+    let person = {
+        'firstName': fName,
+        'lastName': lName,
+        'age': Number(age),
+    }
+
+    return person;
+}
+
+thirdPerson("Peter",
+    "Pan",
+    "20");
+
+function forthPerson(firstName, lastName, age) {
+
+    let person = {};
+
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = Number(age);
+
+    console.log(person);
+
+}
+
+forthPerson("Peter",
+    "Pan",
+    "20");
