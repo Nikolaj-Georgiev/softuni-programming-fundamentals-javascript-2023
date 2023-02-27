9b87edbee9560e3ca25b641d922777544127539a
NGNikolay <georgiev.nikolaj@gmail.com>
2023-02-27 15:14:52 +0200

    Update 07-class-vehicle.js

--- objects-and-classes-exercise/07-class-vehicle.js
+++ objects-and-classes-exercise/07-class-vehicle.js
@@ -17,13 +17,13 @@ class Vehicle {
     }
 }
 
-// let parts = { engine: 6, power: 100 };
-// let vehicle = new Vehicle('a', 'b', parts, 200);
-// vehicle.drive(100);
-// console.log(vehicle.fuel);
-// console.log(vehicle.parts.quality);
-
-let parts = { engine: 9, power: 500 };
-let vehicle = new Vehicle('l', 'k', parts, 840);
-vehicle.drive(20);
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
 console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
+
+let parts1 = { engine: 9, power: 500 };
+let vehicle1 = new Vehicle('l', 'k', parts1, 840);
+vehicle.drive(20);
+console.log(vehicle1.fuel);
