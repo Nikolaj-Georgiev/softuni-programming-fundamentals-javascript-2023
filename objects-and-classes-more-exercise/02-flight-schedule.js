ec90f3e991b6a1c5459471fe6b01db677d821bfc
NGNikolay <georgiev.nikolaj@gmail.com>
2023-03-03 11:12:19 +0200

    Update 02-flight-schedule.js

--- objects-and-classes-more-exercise/02-flight-schedule.js
+++ objects-and-classes-more-exercise/02-flight-schedule.js
@@ -1,44 +1,85 @@
 function flightSchedule(input) {
 
-    let initialFLigth = {};
-    let changedStatuses = {};
-    let action = input[2];
+    let initialFlights = {};
+    let canceledFlights = {};
+    let action = input[2][0];
 
-    arrToObj(input[0], initialFLigth);
-    arrToObj(input[1], changedStatuses);
+    arrToObj(input[0], initialFlights);
+    arrToObj(input[1], canceledFlights);
 
-    console.log(initialFLigth);
-    console.log(changedStatuses);
+    let initialFlightsKeys = Object.keys(initialFlights);
+    let canceledFlightsKeys = Object.keys(canceledFlights);
 
-    for (let key in object) {
-        if (Object.hasOwnProperty.call(object, key)) {
-            const element = object[key];
-            
-        }
+    if (action === 'Cancelled') {
+        initialFlightsKeys.forEach(flight => {
+            canceledFlightsKeys.forEach(cancFl => {
+                if (flight === cancFl) {
+                    console.log(`{ Destination: '${initialFlights[flight]}', Status: 'Cancelled' }`);
+
+                }
+            })
+        });
+
+    } else {
+        canceledFlightsKeys.forEach(flight => {
+            if (flight in initialFlights) {
+                delete initialFlights[flight];
+                index = initialFlightsKeys.indexOf(flight);
+                initialFlightsKeys.splice(index, 1);
+            }
+        });
+        initialFlightsKeys.forEach(flight => {
+            console.log(`{ Destination: '${initialFlights[flight]}', Status: 'Ready to fly' }`);
+        })
+
     }
 
     function arrToObj(arr, obj) {
         arr.forEach(flight => {
-            let [key, value] = flight.split(' ');
+            let currentLine = flight.split(' ');
+            let key = currentLine.shift();
+            let value = currentLine.join(' ');
             obj[key] = value;
         });
     }
 }
 
 flightSchedule([
-['WN269 Delaware',
-    'FL2269 Oregon',
-    'WN498 Las Vegas',
-    'WN3145 Ohio',
-    'WN612 Alabama',
-    'WN4010 New York',
-    'WN1173 California',
-    'DL2120 Texas',
-    'KL5744 Illinois',
-    'WN678 Pennsylvania'],
-['DL2120 Cancelled',
-    'WN612 Cancelled',
-    'WN1173 Cancelled',
-    'SK430 Cancelled'],
-['Cancelled']
+    ['WN269 Delaware',
+        'FL2269 Oregon',
+        'WN498 Las Vegas',
+        'WN3145 Ohio',
+        'WN612 Alabama',
+        'WN4010 New York',
+        'WN1173 California',
+        'DL2120 Texas',
+        'KL5744 Illinois',
+        'WN678 Pennsylvania'
+    ],
+    ['DL2120 Cancelled',
+        'WN612 Cancelled',
+        'WN1173 Cancelled',
+        'SK430 Cancelled'
+    ],
+    ['Cancelled']
 ]);
+
+// flightSchedule([
+//     ['WN269 Delaware',
+//         'FL2269 Oregon',
+//         'WN498 Las Vegas',
+//         'WN3145 Ohio',
+//         'WN612 Alabama',
+//         'WN4010 New York',
+//         'WN1173 California',
+//         'DL2120 Texas',
+//         'KL5744 Illinois',
+//         'WN678 Pennsylvania'
+//     ],
+//     ['DL2120 Cancelled',
+//         'WN612 Cancelled',
+//         'WN1173 Cancelled',
+//         'SK330 Cancelled'
+//     ],
+//     ['Ready to fly']
+// ]);
