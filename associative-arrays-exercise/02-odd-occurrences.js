9540cf144d44ef60f2afbfbcb0ce5c8b6b2ffb96
NGNikolay <georgiev.nikolaj@gmail.com>
2023-03-07 17:03:39 +0200

    Create 02-odd-occurrences.js

---
+++ associative-arrays-exercise/02-odd-occurrences.js
@@ -0,0 +1,30 @@
+function oddOcc(string) {
+
+    let holdingObject = {};
+
+    string.toLowerCase().split(' ').forEach(string => {
+        if (string in holdingObject) {
+            holdingObject[string]++;
+        } else {
+            holdingObject[string] = 1;
+        }
+    });;
+
+    let resultArr = [];
+    resultArr = Object.entries(holdingObject)
+        .filter(x => x[1] % 2 !== 0)
+        .sort((a, b) => b[1] - a[1])
+        .map(x => x.splice(0, 1));
+
+
+
+    console.log(resultArr.join(' '));
+
+
+
+
+}
+
+oddOcc('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
+console.log('++++++++++++++++++++++++++');
+oddOcc('Cake IS SWEET is Soft CAKE sweet Food');
