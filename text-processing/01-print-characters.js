e9187a003ec06d7f99767018716b83de572c7488
NGNikolay <georgiev.nikolaj@gmail.com>
2023-03-20 14:39:18 +0200

    Create 01-print-characters.js

---
+++ text-processing/01-print-characters.js
@@ -0,0 +1,23 @@
+function printCharacters(string) {
+
+    for (let i = 0, len = string.length; i < len; i++) {
+        console.log(string[i]);
+    }
+
+}
+
+printCharacters('AWord');
+console.log('++++++++++++++++');
+printCharacters('Sentence');
+console.log('++++++++++++++++');
+
+function printCharacters1(string) {
+
+    for (let char of string) {
+        console.log(char);
+    }
+
+}
+printCharacters1('AWord');
+console.log('++++++++++++++++');
+printCharacters1('Sentence');
