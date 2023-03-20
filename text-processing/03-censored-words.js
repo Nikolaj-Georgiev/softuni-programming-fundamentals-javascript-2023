00be743ba35616ab2a3a7f6b8cf3310496e69070
NGNikolay <georgiev.nikolaj@gmail.com>
2023-03-20 21:33:18 +0200

    Update 03-censored-words.js

--- text-processing/03-censored-words.js
+++ text-processing/03-censored-words.js
@@ -1,14 +1,36 @@
-function censoredWord(text, searchedWord) {
+// function censoredWord(text, searchedWord) {
+
+//     let resultString = text;
+
+//     while (resultString.includes(searchedWord)) {
+//         resultString = resultString.replace(searchedWord, ('*'.repeat(searchedWord.length)));
+//     }
+
+//     console.log(resultString);
+// }
+
+// censoredWord('A small sentence with some words', 'small');
+// console.log('++++++++++++++++++++++++++');
+// censoredWord('Find the hidden word hidden pesho hidden.', 'hidden');
+// console.log('++++++++++++++++++++++++++');
+
+
+function censoredWord1(text, searchedWord) {
 
     let resultString = text;
+    let censoredWord = '*'.repeat(searchedWord.length);
+    let index = resultString.indexOf(searchedWord);
 
-    while (resultString.includes(searchedWord)) {
-        resultString = resultString.replace(searchedWord, ('*'.repeat(searchedWord.length)));
+    while (index >= 0) {
+        let startLocal = resultString.substring(0, index);
+        let endLocal = resultString.substring(index + searchedWord.length);
+        resultString = startLocal + censoredWord + endLocal;
+        index = resultString.indexOf(searchedWord);
     }
 
     console.log(resultString);
 }
 
-censoredWord('A small sentence with some words', 'small');
+censoredWord1('A small sentence with some words', 'small');
 console.log('++++++++++++++++++++++++++');
-censoredWord('Find the hidden word hidden pesho hidden.', 'hidden');
+censoredWord1('Find the hidden word hidden pesho hidden.', 'hidden');
