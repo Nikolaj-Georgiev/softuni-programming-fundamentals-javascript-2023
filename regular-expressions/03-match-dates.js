489dfed6faac8c82e820ee173d7ed6d81dc2856e
NGNikolay <georgiev.nikolaj@gmail.com>
2023-03-27 11:04:50 +0300

    Create 03-match-dates.js

---
+++ regular-expressions/03-match-dates.js
@@ -0,0 +1,15 @@
+function matchDates(input) {
+
+    let stringOfDates = input[0];
+    let pattern = /\b\d{2}([.\/-]+?)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b/g;
+    let execResult = pattern.exec(stringOfDates);
+    while (execResult !== null) {
+        let day = execResult[0].split(execResult[1])[0];
+        console.log(`Day: ${day}, Month: ${execResult.groups.month}, Year: ${execResult.groups.year}`);
+        execResult = pattern.exec(stringOfDates);
+    }
+}
+
+matchDates(['13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016']);
+console.log('++++++++++++++');
+matchDates(['1/Jan-1951 23/october/197 11-Dec-2010 18.Jan.2014']);
